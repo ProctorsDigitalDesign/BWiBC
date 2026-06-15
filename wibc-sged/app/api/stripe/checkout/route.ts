71e04262712e722287ad720a5a2ff21767f40487
KamilArtem <kamilwejwoda@gmail.com>
2026-06-15 14:29:22 +0200

    fix(stripe): update apiVersion to bypass typescript error

--- wibc-sged/app/api/stripe/checkout/route.ts
+++ wibc-sged/app/api/stripe/checkout/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY || "";
 const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2026-05-27.dahlia" as any,
 });
 
 export async function POST(req: NextRequest) {
